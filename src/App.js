6ffe947197cdda7d51a544cd38a607e1d901c264
Max Chen <csl199254@gmail.com>
2020-03-30 11:34:53 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-      <h1>Create React App + Go API</h1>
-  );
+  return <h1>Crasdasdasdasdasdaasasdeate React App + Go API</h1>;
 }
 
 export default App;
